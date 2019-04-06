4c3ab5ce19198edcb2cadd6d1f641a6c3a21c01a
chenggongzhao <gongzhao.cheng@rayjoy.com>
2019-04-06 15:40:06 +0800

    1.找到控件

--- app/src/main/java/com/cgz/multidownloader/MainActivity.java
+++ app/src/main/java/com/cgz/multidownloader/MainActivity.java
@@ -2,12 +2,32 @@ package com.cgz.multidownloader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText mEtPath;
+    public EditText mEtThreadCount;
+    public LinearLayout mLLContainer;
+    public Button mBtSelf;
+    public Button mBtOther;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 1.找到控件
+        // 2.在button点击事件当中下载文件
+        // 3.下载的时候在界面显示下载的进度
+        mEtPath = findViewById(R.id.et_path);
+        mEtThreadCount = findViewById(R.id.et_threadcount);
+        mLLContainer = findViewById(R.id.ll_container);
+        mBtSelf =  findViewById(R.id.bt_self);
+        mBtOther = (Button) findViewById(R.id.bt_other);
+
+
     }
 }
